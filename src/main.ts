99f675f165b4b1103dfbf32394e4d02bda8e29b3
lithaxor <officials.hasan@gmail.com>
2023-10-14 14:54:52 +0600

    property save api added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { PORT } from "./configs";
 import express from "express";
 import { setupDatabase } from "./database";
 import searchRouter from "./routes/search.router";
+import propertyRouter from "./routes/property.router";
 
 async function bootstrap() {
   const app = express();
@@ -10,6 +11,7 @@ async function bootstrap() {
   app.use(express.json());
 
   app.use("/api/search", searchRouter);
+  app.use("/api/property", propertyRouter);
 
   app.listen(PORT, () => {
     console.log("Server listening on port " + PORT);
