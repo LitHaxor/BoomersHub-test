f0b52a45835b3dbdce757e74d410fe88caaeba5c
lithaxor <officials.hasan@gmail.com>
2023-10-15 23:50:44 +0600

    frontend wired bug on axios

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     domains: ["property-images-se-1.s3.amazonaws.com"],
   },
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,6 +5,9 @@ import type { AppProps } from "next/app";
 import theme from "../theme.confg";
 import LayoutHeader from "@/components/Layout/Header";
 import Loading from "@/components/Layout/Loading";
+import { baseApi } from "@/baseApi";
+
+baseApi.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
